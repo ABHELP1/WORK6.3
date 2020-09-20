efea51a4bae1e44ca92de6152fa0967e401f7397
channasorn chungmarisakul <63010185@KMITL.AC.TH>
2020-09-20 19:53:00 +0700

    Add project files.

---
+++ WORK6.3/Source.cpp
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int i;
+int num[3];
+int x = 0;
+int max;
+int a = 0;
+int count = 0;
+int main()
+{
+
+    for (i = 0; i < 3; i++)
+    {
+        scanf("%d", &num[i]);
+    }
+    for (i = 0; i < 3; i++)
+    {
+        max = num[i];
+        while (max != 1)
+        {
+            if (max % 2 == 0)
+            {
+                max = max / 2;
+                count++;
+            }
+            else if (max % 2 == 1)
+            {
+                max = max - 1;
+                max = max / 2;
+                count++;
+            }
+        }
+    }
+    printf("%d", count);
+    return 0;
+}
